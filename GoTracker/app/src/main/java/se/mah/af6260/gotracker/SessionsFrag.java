dc1b0316e3cbb322ba5d318164b1ce5479af0973
oggez <oskarzetterstrom@hotmail.com>
2017-03-07 11:58:41 +0100

    tabs, calendar osv

---
+++ GoTracker/app/src/main/java/se/mah/af6260/gotracker/SessionsFrag.java
@@ -0,0 +1,39 @@
+package se.mah.af6260.gotracker;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SessionsFrag extends Fragment {
+    private CalendarView cv;
+
+    public SessionsFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v= inflater.inflate(R.layout.fragment_sessions, container, false);
+        cv = (CalendarView)v.findViewById(R.id.calendarView);
+        cv.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int years, int months, int dayOfMonth) {
+
+            }
+        });
+        return v;
+    }
+
+}
