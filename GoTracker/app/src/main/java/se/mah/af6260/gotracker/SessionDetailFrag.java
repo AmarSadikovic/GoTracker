834320bcf665e9dc96071ebee13f832fb2eb4df3
oggez <oskarzetterstrom@hotmail.com>
2017-03-16 11:01:51 +0100

    IT WORKS

--- GoTracker/app/src/main/java/se/mah/af6260/gotracker/SessionDetailFrag.java
+++ GoTracker/app/src/main/java/se/mah/af6260/gotracker/SessionDetailFrag.java
@@ -1,6 +1,8 @@
 package se.mah.af6260.gotracker;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -8,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -62,6 +65,26 @@ public class SessionDetailFrag extends Fragment implements OnMapReadyCallback {
         tvStartime.setText("Start time : " + session.getStartTime());
         tvAverageSpeed.setText("Average Speed : " + session.getAvgSpeed());
         route = session.getRouteArray();
+        Button btn = (Button)v.findViewById(R.id.sessionDelete);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage("Do you really want to delete this session?")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
         return v;
     }
 
